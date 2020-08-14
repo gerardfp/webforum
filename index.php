18d0273aef856d2f049b2d66c45fe76981a5efb2
root <root@myblog.lxd>
2020-08-14 14:28:41 +0000

    webforum ok (algo de css)

--- index.php
+++ index.php
@@ -1,10 +1,34 @@
-<link rel="stylesheet" href="style.css">
-<h1>MY BLOG!</h1>
-<a href='new_post.php'>Crear nuevo post</a>
+<h1>FORUM</h1>
 <style>
+html {
+	font-family: sans;
+}
+
 details {
-	margin: 2em;
-	border: 1px solid gray;
+	margin: 24px;
+	border-left: 1px solid blue;
+}
+
+summary {
+	background-color: #e1e1e1;
+	padding: 16px;
+}
+
+.reply summary {
+	list-style: none;
+	list-style-image: none; //url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgeD0iMHB4IiB5PSIwcHgiIHZpZXdCb3g9IjAgMCAxMDAgMTAwIj48cGF0aCBkPSJNOTAuNiwySDkuNGMtMS4zLDAtMi4zLDEtMi4zLDIuM1Y3NmMwLDEuMywxLDIuMywyLjMsMi4zaDI2LjFMNTAsOThsMTQuNS0xOS43aDI2LjFjMS4zLDAsMi4zLTEsMi4zLTIuM1Y0LjMgIEM5Mi45LDMuMSw5MS45LDIsOTAuNiwyeiBNNzIuMSw0NS4xbC00LjUsMGwtMjAuOSwwbDguNiw4LjdsLTcsNi45TDM0LjEsNDYuNGwtNi4yLTYuMmwwLDBsMCwwbDEuOC0xLjhsNS4yLTUuMWwwLDBsNy4zLTcuMyAgbDYuMi02LjJsNi45LDdsLTYuMiw2LjJsLTIuNCwyLjRsMjAuOCwwbDQuNiwwTDcyLjEsNDUuMXoiLz48L3N2Zz4K");
+	padding: 4px;
+	cursor: pointer;
+}
+
+.reply {
+	border: 0;
+	padding: 4px;
+	margin: 0px;
+}
+
+input {
+	height: 3em;
 }
 </style>
 
@@ -26,10 +50,10 @@ function buildTree(array $rows, $replyto = NULL) {
 
     	foreach ($rows as $row) {
         	if ($row['replyto'] == $replyto) {
-			echo "<details>";
+			echo "<details open>";
             		echo "<summary>";
 			echo $row['id'] . " :: " . $row['post'] . " :: " . $row['replyto'];
-			echo "<details><summary>Reply</summary><form action='?'><input type='hidden' name='replyto' value='" . $row['id'] ."'><input type='text' name='post'><input type='submit'></form></details>";
+			echo "<details class='reply'><summary>Responder</summary><form action='?'><input type='hidden' name='replyto' value='" . $row['id'] ."'><input type='text' name='post'><input type='submit' value='Responder'></form></details>";
 			echo "</summary>";
 			$children = buildTree($rows, $row['id']);
 			echo "</details>";
