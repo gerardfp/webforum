87fc607dbf022eb749fdb8608049079ad535f239
root <root@myblog.lxd>
2020-08-17 09:53:39 +0000

    ok

--- index.php
+++ index.php
@@ -1,77 +1,97 @@
-<h1>FORUM</h1>
 <style>
 html {
 	font-family: sans;
 }
 
+h1 {
+	background-color: #e35614;
+	color: white;
+	padding: 16px;
+}
+
+body > details {
+	border: 1px solid #e35614;
+	margin-left: 0;
+}
+
 details {
-	margin: 24px;
-	border-left: 1px solid blue;
+	margin: 24px 0px 24px 24px;
+	border-left: 1px solid #e35614;
 }
 
 summary {
-	background-color: #e1e1e1;
-	padding: 16px;
+	background-color: #fff5ed;
+	padding: 16px 16px 0px 16px;
 }
 
 .reply summary {
 	list-style: none;
-	list-style-image: none; //url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgeD0iMHB4IiB5PSIwcHgiIHZpZXdCb3g9IjAgMCAxMDAgMTAwIj48cGF0aCBkPSJNOTAuNiwySDkuNGMtMS4zLDAtMi4zLDEtMi4zLDIuM1Y3NmMwLDEuMywxLDIuMywyLjMsMi4zaDI2LjFMNTAsOThsMTQuNS0xOS43aDI2LjFjMS4zLDAsMi4zLTEsMi4zLTIuM1Y0LjMgIEM5Mi45LDMuMSw5MS45LDIsOTAuNiwyeiBNNzIuMSw0NS4xbC00LjUsMGwtMjAuOSwwbDguNiw4LjdsLTcsNi45TDM0LjEsNDYuNGwtNi4yLTYuMmwwLDBsMCwwbDEuOC0xLjhsNS4yLTUuMWwwLDBsNy4zLTcuMyAgbDYuMi02LjJsNi45LDdsLTYuMiw2LjJsLTIuNCwyLjRsMjAuOCwwbDQuNiwwTDcyLjEsNDUuMXoiLz48L3N2Zz4K");
 	padding: 4px;
 	cursor: pointer;
+	color: #e35614;
 }
 
 .reply {
 	border: 0;
 	padding: 4px;
-	margin: 0px;
+	margin: 0;
 }
 
 input {
 	height: 3em;
 }
+
+input[type=submit] {
+	color: #e35614;
+}
 </style>
+
+<h1>&#128024; foro</h1>
+<form action="?">
+	<input type="text" name="post" size="32">
+	<input type="submit" value="Publicar">
+</form>
 
 <?php
 $mysqli = new mysqli("localhost", "webforum_user", "webforum_password", "webforum_database");
 
 if(isset($_GET['post'])){
-	$stmt = $mysqli->prepare("INSERT INTO posts (post, replyto) VALUES (?,?)");
-
-	$stmt->bind_param("ss", $_GET['post'], $_GET['replyto']);
-
+	$stmt = $mysqli->prepare("INSERT INTO posts (post, replyto) VALUES (?,NULL)");
+	$stmt->bind_param("s", $_GET['post']);
 	$stmt->execute();
 	$stmt->close();
-
 }
+
+if(isset($_GET['reply'])){
+	$stmt = $mysqli->prepare("INSERT INTO posts (post, replyto) VALUES (?,?)");
+	$stmt->bind_param("ss", $_GET['reply'], $_GET['replyto']);
+	$stmt->execute();
+	$stmt->close();
+}
+
+$result = $mysqli->query("SELECT * FROM posts");
+
+buildTree($result->fetch_all(MYSQLI_ASSOC));
 
 function buildTree(array $rows, $replyto = NULL) {
-    	$branch = array();
-
     	foreach ($rows as $row) {
         	if ($row['replyto'] == $replyto) {
-			echo "<details open>";
-            		echo "<summary>";
-			echo $row['id'] . " :: " . $row['post'] . " :: " . $row['replyto'];
-			echo "<details class='reply'><summary>Responder</summary><form action='?'><input type='hidden' name='replyto' value='" . $row['id'] ."'><input type='text' name='post'><input type='submit' value='Responder'></form></details>";
-			echo "</summary>";
-			$children = buildTree($rows, $row['id']);
+			echo "
+				<details open>
+            				<summary>{$row['post']}
+						<details class='reply'>
+							<summary>&#8617;</summary>
+							<form action='?'>
+								<input type='hidden' name='replyto' value='{$row['id']}'>
+								<input type='text' name='reply' size='32'>
+								<input type='submit' value='Responder'>
+							</form>
+						</details>
+					</summary>
+			";
+			buildTree($rows, $row['id']);
 			echo "</details>";
-            		if ($children) {
-                		$row['children'] = $children;
-            		}
-            		$branch[] = $row;
 		}
     	}
-
-    	return $branch;
 }
-
-$res = $mysqli->query("SELECT * FROM posts");
-
-while($row = $res->fetch_assoc()){
-	$rows[] = $row;
-}
-
-buildTree($rows);
 ?>
